44a1a9e231233e433b433ded02ffc6f6e98c83fd
Andreivan <andreivan.santos@gmail.com>
2020-07-12 15:43:52 +0200

    Implement jwt authentication

---
+++ src/main/java/com/jwt/resource/BcryptEncoderTest.java
@@ -0,0 +1,16 @@
+package com.jwt.resource;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptEncoderTest {
+
+    public static void main(String[] args) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        for(int i = 0; i <= 10; i++) {
+            String encodedString = encoder.encode("asd123");
+            System.out.println(encodedString);
+        }
+    }
+}
