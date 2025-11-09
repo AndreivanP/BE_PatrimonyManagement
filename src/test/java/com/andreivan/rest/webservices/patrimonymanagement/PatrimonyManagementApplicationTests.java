18024028142dc28d46ddb5fd6c9487b6fee54b71
AndreivanP <andreivan.santos@gmail.com>
2025-11-08 22:06:26 -0300

    Render

---
+++ src/test/java/com/andreivan/rest/webservices/patrimonymanagement/PatrimonyManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.andreivan.rest.webservices.patrimonymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PatrimonyManagementApplication.class)
+class PatrimonyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
